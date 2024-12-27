756629d17f2c39c231ab6682049178049019fa30
henrique.bilo <henrique-bilo@hotmail.com>
2024-12-27 00:04:05 -0300

    Created the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'LittleStore';
