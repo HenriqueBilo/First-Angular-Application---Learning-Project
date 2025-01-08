a31cdb81755447fd80944f825605f44ded768d61
henrique.bilo <henrique-bilo@hotmail.com>
2025-01-08 00:26:27 -0300

    Training routes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from './navigation/menu/menu.component';
-import { HomeComponent } from './navigation/home/home.component';
 import { FooterComponent } from './navigation/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [MenuComponent, HomeComponent, FooterComponent],
+  imports: [
+    MenuComponent, FooterComponent, RouterOutlet
+  ],
 })
 export class AppComponent {
   title = 'LittleStore';
