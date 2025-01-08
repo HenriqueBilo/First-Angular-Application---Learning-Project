3c57df440f2bf3e82078fafa567cab4f582e7ffd
henrique.bilo <henrique-bilo@hotmail.com>
2025-01-07 23:32:32 -0300

    Improving navigation components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from './navigation/menu/menu.component';
+import { HomeComponent } from './navigation/home/home.component';
+import { FooterComponent } from './navigation/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  imports: [MenuComponent, HomeComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'LittleStore';
