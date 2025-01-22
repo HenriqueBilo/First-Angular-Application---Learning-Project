8ac2ee9daf593434cbe9bd835ee974c2989b9061
henrique.bilo <henrique-bilo@hotmail.com>
2025-01-22 20:41:31 -0300

    Testing pipes and structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,12 @@ import { MenuComponent } from './navigation/menu/menu.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { ProductService } from './products/products.service';
-//import { HttpClientModule } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   imports: [
-    MenuComponent, FooterComponent, RouterOutlet, //HttpClientModule
+    MenuComponent, FooterComponent, RouterOutlet,
   ],
   providers: [ProductService],
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
